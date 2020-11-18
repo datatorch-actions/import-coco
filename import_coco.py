6d71060bfe7aa68ad9c8fa255661ba7b81875395
Justin Brooks <jsbroks@gmail.com>
2020-11-18 16:46:44 -0500

    chore: rename getters

--- import_coco.py
+++ import_coco.py
@@ -1,14 +1,16 @@
 from datatorch.api.scripts.import_coco import import_coco
 from datatorch import get_inputs
 
+inputs = get_inputs()
+
 if __name__ == "__main__":
-    file_path: str = get_inputs("path")
-    project: str = get_inputs("project")
-    import_bbox: bool = get_inputs("bbox")
-    import_segmentation: bool = get_inputs("segmentation")
-    max_iou: float = get_inputs("maxIou")
-    simplify: float = get_inputs("simplify")
-    ignore_annotations_with_ids: bool = get_inputs("ignoreAnnotationsWithIds")
+    file_path: str = inputs.get("path")
+    project: str = inputs.get("project")
+    import_bbox: bool = inputs.get("bbox")
+    import_segmentation: bool = inputs.get("segmentation")
+    max_iou: float = inputs.get("maxIou")
+    simplify: float = inputs.get("simplify")
+    ignore_annotations_with_ids: bool = inputs.get("ignoreAnnotationsWithIds")
 
     # Call the importer already built into the python SDK at
     # `datatorch import coco <args>`
